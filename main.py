d8642123b7f9d0d6fab5e0c581bb7a89a62bf961
cypherlux <89822852+cypherlux@users.noreply.github.com>
2023-02-25 11:36:10 -0700

    Update main.py
    the most updated version as of today

--- main.py
+++ main.py
@@ -1,65 +1,55 @@
 #let's gooooooo!
-
 import random
 import time
 from pyfiglet import Figlet
 
-
-def intro_text_font():
-    
-    dope_font = Figlet(font='slant')
-    #dope_font = Figlet(font='5lineoblique')
-
-    print(dope_font.renderText('the'))
-    print(dope_font.renderText('oracle'))
-
-
-
-# 8 BALL RESPONSES / INTERACTIONS
+#lists of responses/interactions
 intro_phrase = [
-    "hiii there :)",
-    "howdy",
-    "hello :D",
-    "yoooo",
+    "Hiii there :)",
+    "Howdy",
+    "Hello :D",
+    "Yoooo",
     "Hey!",
     "Ahoy!",
     "Heyyyy!",
+    "Whuddup!"
 ]
 
-
 offering_advice = [
-    "how can I help you?",
+    "How can I help you?", 
     "Whatcha need help with?",
-    "what can the oracle provide?",
+    "What can the oracle provide?",
     "I'm here. What's up?"
 ]
 
-
 thinking_out_loud = [
-    "wow that's a tough one",
-    "give me a sec",
-    "give me a sec...",
-    "...",
-    "hmmmm",
-    "hmm...."
+    "Wow that's a tough one",
+    "Give me a sec",
+    "Give me a sec...",
+    "...", 
+    "Hmmmm",
+    "Hmm...."
 ]
 
-
 final_answer = [
-    "hmmmmm....no <3",
+    "Hmmmmm....no <3",
     ";)",
     "YES",
     "NO",
-    "not sure",
+    "Not sure",
+    "¯\_ (ツ)_/¯",
     "¯\_ (ツ)_/¯",
     "¯\_ (ツ)_/¯",
     "Teeheehe, what do you think?",
     "yep :)",
     "I don't know...",
     "Yep, you nailed it.",
-    "of course!!"
+    "idk"
+    "Heck yeah!!!"
+    "Of course!!",
+    "Definitely!",
+    "Mos def"
 ]
-
 
 wanna_play_again = [
     "Do you have another question to ask?",
@@ -71,66 +61,108 @@ wanna_play_again = [
     "need more clarity?"
 ]
 
-
 bye = [
     "byeeeee!",
     "bye <3",
     "until next time <3",
     "farewell!",
-    "see ya!"
- ]
+    "see ya!"]
 
 
+
+#fancy "the oracle" font
+def intro_text_font():
+
+    dope_font = Figlet(font='slant')
+    
+    print(dope_font.renderText('the'))
+    print(dope_font.renderText('oracle'))    
+
+    
+
+# should be self-explanatory lol
+def crystal_ball_image():
+    print("            ⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⠾⠛⠋⠉⠉⠉⠉⢙⣿⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀")
+    print(" ⠀⠀⠀⠀⠀⠀⢀⣼⠟⠁⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀")
+    print(" ⠀⠀⠀⠀⠀⢠⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⡟⣷⡀⠀⠀⠀⠀⠀")
+    print(" ⠀⠀⠀⠀⠀⣾⢇⣤⣶⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠙⠛⠛⠁⢹⣇⠀⠀⠀⠀⠀")
+    print(" ⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⡀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀")
+    print(" ⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⢸⡏⠀⠀⠀⠀⠀")
+    print(" ⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀⢠⡿⠁⠀⠀⠀⠀⠀")
+    print(" ⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⢀⣴⠟⠁⠀⠀⠀⠀⠀⠀")
+    print(" ⠀⠀⠀⠀⠀⠀⣠⣤⡙⠻⢿⣿⣿⣿⣿⣿⣋⣠⣤⡶⠟⢁⣤⡄⠀⠀⠀⠀⠀⠀")
+    print(" ⠀⠀⠀⠀⠀⠀⢿⣿⣿⣷⣤⣈⣉⠉⠛⠛⠉⣉⣠⣤⣾⣿⣿⡟⠀⠀⠀⠀⠀⠀")
+    print(" ⠀⠀⠀⠀⣾⣦⣀⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⢋⣠⣴⣷⠀⠀⠀⠀")
+    print(" ⠀⠀⠀⠀⢿⣿⣿⣿⣷⣶⣤⣬⣭⣉⣉⣉⣩⣭⣥⣤⣶⣾⣿⣿⣿⡿⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⠀⠀⠀⠀⠀")
+    print(" ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠛⠛⠛⠛⠛⠋⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+
+
+
+
+#the start of the oracle's life
 def awakening():
-    
+
     intro_text_font()
-    time.sleep(1)
+    time.sleep(2)
+    crystal_ball_image()
+    time.sleep(2)
     
+    print(
+    "\nWelcome to the magical Oracle, where you can ask any question that your heart desires."
+  )
+    time.sleep(4)
+    print("Actually, any yes/no* question your heart desires :)")
+    time.sleep(3)
+    print("because that's all I'm trained to do at this point...")
+    time.sleep(3)
+    print("...anyway...")
+    time.sleep(1)
+
     intro_phrase_prompt = random.choice(intro_phrase)
     print("\n",intro_phrase_prompt, "\n")
-    time.sleep(2)
+    time.sleep(1)
 
-    offering_advice_response = random.choice(offering_advice)
-    print("\n",offering_advice_response, "\n")
-    print("    [press enter] \n")
-    time.sleep(4)
+    print("\n",random.choice(offering_advice), "\n")
+    print("*** Think about your question, then press [enter] to 'ask' *** \n")
+
 
 awakening()
 
 
-
+#pressing enter to repeat the cycle of asking a question
 def asking_the_question():
-    
+
     while True:
-
-        enter_is_pressed = input("")
-
-        thinking_out_loud_reponse = random.choice(thinking_out_loud)
-        print()
-        print(thinking_out_loud_reponse)
-        print()
-              
+    
+        user_input = input("> ")
         
-        final_answer_reponse = random.choice(final_answer)
+        # if user pressed [enter]  
+        if user_input == "":
+            # the oracle is thinking...
+            time.sleep(1)
+            print("\n",random.choice(thinking_out_loud))
         
-        print()
-        time.sleep(1)
-        print(final_answer_reponse)
-        time.sleep(1)
-        print()
+            # the oracle is giving its answer
+            time.sleep(2)
+            print("\n",random.choice(final_answer),"\n")
         
-        wanna_play_again_response = random.choice(wanna_play_again)
+            # the oracle asks if user has another question
+            print()
+            time.sleep(2)
+            print(random.choice(wanna_play_again))
+            print("\n*** Press [enter] to ask again or [q] to end ***\n")
         
-        print()
-        time.sleep(1)
-        print(wanna_play_again_response)
-        print("    [press enter]")
-        print()
-
-        time.sleep(7)
-
-        bye_response = random.choice(bye)
-        print("\n",bye_response, "\n")
-
+        # if user types [Q] or [q]  
+        elif user_input == "Q" or user_input == "q":
+            #the oracle says bye
+            print("\n",random.choice(bye), "\n")
+            break
+        
+        # if user types something besides [enter] or [Q] or [q]
+        else:
+            print("\nOnly press the [enter] or [q] keys <3\n")
+    
 
 asking_the_question()
